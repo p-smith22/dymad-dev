Augmented SA test with different ls solvers. Exposed bugs in numerical stability in SAKO pseudospectrum; fixed by eigenvalue clipping. Also a minor bug in _ct_full_log.

scripts/sa_lti/lti_sa.py
@@ -20,14 +20,14 @@ dt = t_grid[1] - t_grid[0]
 # A = np.array([
 #     [0.0, 1.0],
 #     [-4.0, 0.0]])
-# prf = 'lti_dmp'
-# A = np.array([
-#     [0.0, 1.0],
-#     [-4.0, -1.0]])
-prf = 'lti_dgn'
-A = 0.5*np.array([
-    [-1.0,-0.9],
-    [0.0, -1.0]])
+prf = 'lti_dmp'
+A = np.array([
+    [0.0, 1.0],
+    [-4.0, -1.0]])
+# prf = 'lti_dgn'
+# A = 0.5*np.array([
+#     [-1.0,-0.9],
+#     [0.0, -1.0]])
 
 def f(t, x):
     return (x @ A.T)
@@ -156,11 +156,11 @@ if ifprd:
         labels=['Truth'] + labels, ifclose=False)
 
 if ifint:
-    sact = SpectralAnalysis(KBF,  'sa_kbf_nd.pt',  dt=dt, reps=1e-10)
-    sand = SpectralAnalysis(DKBF, 'sa_dkbf_nd.pt', dt=dt, reps=1e-10)
-    saln = SpectralAnalysis(DKBF, 'sa_dkbf_ln.pt', dt=dt, reps=1e-10)
-    satr = SpectralAnalysis(DKBF, 'sa_dkbf_tr.pt', dt=dt, reps=1e-10)
-    sasa = SpectralAnalysis(DKBF, 'sa_dkbf_sa.pt', dt=dt, reps=1e-10)
+    sact = SpectralAnalysis(KBF,  'sa_kbf_nd.pt',  dt=dt, reps=1e-10, etol=1e-12)
+    sand = SpectralAnalysis(DKBF, 'sa_dkbf_nd.pt', dt=dt, reps=1e-10, etol=1e-12)
+    saln = SpectralAnalysis(DKBF, 'sa_dkbf_ln.pt', dt=dt, reps=1e-10, etol=None)
+    satr = SpectralAnalysis(DKBF, 'sa_dkbf_tr.pt', dt=dt, reps=1e-10, etol=None)
+    sasa = SpectralAnalysis(DKBF, 'sa_dkbf_sa.pt', dt=dt, reps=1e-10, etol=None)
 
     sas = [saln, satr, sasa, sand, sact]
     lbs = ['DT-LN', 'DT-TR', 'DT-SA', 'DT-ND', 'CT-ND']
@@ -188,9 +188,9 @@ if ifint:
             pss, psk = [], []
             for _s in sas:
                 grid, _pss = _s.estimate_ps(gg, mode='disc', method='standard', return_vec=False)
-                # grid, _psk = _s.estimate_ps(gg, mode='disc', method='sako', return_vec=False)
+                grid, _psk = _s.estimate_ps(gg, mode='disc', method='sako', return_vec=False)
                 pss.append((grid, _pss))
-                # psk.append((grid, _psk))
+                psk.append((grid, _psk))
             # Exact
             psrf = estimate_pseudospectrum(
                 grid, resolvent_analysis, return_vec=False,
@@ -198,10 +198,10 @@ if ifint:
 
             for _i in range(5):
                 grid, _pss = pss[_i]
-                # grid, _psk = psk[_i]
+                grid, _psk = psk[_i]
                 f, ax[_i] = complex_plot(grid, 1/psrf, rng, fig=(f, ax[_i]), mode='line', lwid=0.5, lsty='dashed')
                 f, ax[_i] = complex_plot(grid, 1/_pss, rng, fig=(f, ax[_i]), mode='line', lwid=2, lsty='dotted')
-                # f, ax[_i] = complex_plot(grid, 1/_psk, rng, fig=(f, ax[_i]), mode='line', lwid=1)
+                f, ax[_i] = complex_plot(grid, 1/_psk, rng, fig=(f, ax[_i]), mode='line', lwid=1)
 
     if ifeic:
         ## Eigenvalues
@@ -223,9 +223,9 @@ if ifint:
             pss, psk = [], []
             for _s in sas:
                 grid, _pss = _s.estimate_ps(gg, mode='cont', method='standard', return_vec=False)
-                # grid, _psk = _s.estimate_ps(gg, mode='cont', method='sako', return_vec=False)
+                grid, _psk = _s.estimate_ps(gg, mode='cont', method='sako', return_vec=False)
                 pss.append((grid, _pss))
-                # psk.append((grid, _psk))
+                psk.append((grid, _psk))
             # Exact
             psrf = estimate_pseudospectrum(
                 grid, resolvent_analysis, return_vec=False,
@@ -233,10 +233,10 @@ if ifint:
 
             for _i in range(5):
                 grid, _pss = pss[_i]
-                # grid, _psk = psk[_i]
+                grid, _psk = psk[_i]
                 f, ax[_i] = complex_plot(grid, 1/psrf, rng, fig=(f, ax[_i]), mode='line', lwid=0.5, lsty='dashed')
                 f, ax[_i] = complex_plot(grid, 1/_pss, rng, fig=(f, ax[_i]), mode='line', lwid=2, lsty='dotted')
-                # f, ax[_i] = complex_plot(grid, 1/_psk, rng, fig=(f, ax[_i]), mode='line', lwid=1)
+                f, ax[_i] = complex_plot(grid, 1/_psk, rng, fig=(f, ax[_i]), mode='line', lwid=1)
 
         for _i in range(5):
             ax[_i].set_xlim([-4.0, 0.5])

src/dymad/sako/base.py
@@ -69,7 +69,7 @@ class SpectralAnalysis:
     """
     def __init__(self,
                  model_class: Type[torch.nn.Module], checkpoint_path: str,
-                 forder='full', dt: float = 1.0, reps: float = 1e-10, remove_one=True):
+                 forder='full', dt: float = 1.0, reps: float = 1e-10, remove_one=True, etol: float = 1e-13):
         self._dt = dt
         self._reset()
 
@@ -78,7 +78,7 @@ class SpectralAnalysis:
         self._solve_eigs()
         logger.info(f"Orthonormality violation: {check_orthogonality(self._vl, self._vr)[1]:4.3e}")
         self._proc_eigs()
-        self._sako = SAKO(self._ctx._P0, self._ctx._P1, None, reps=reps)
+        self._sako = SAKO(self._ctx._P0, self._ctx._P1, None, reps=reps, etol=etol)
         self._rals = RALowRank(self._vr, np.diag(self._wc.conj()), self._vl, dt=self._dt)
 
         self.filter_spectrum(forder, remove_one=remove_one)

src/dymad/sako/sako.py
@@ -7,6 +7,14 @@ from dymad.numerics import generate_coef
 
 logger = logging.getLogger(__name__)
 
+def clip_eig(M, tol=1e-13):
+    w, v = spl.eigh(M)
+    msk = w < tol
+    if np.any(msk):
+        logger.info(f"clip_eig: {np.sum(msk)} small eigenvalues found, max {np.max(-w[msk]):4.3e}")
+        w[msk] = tol
+    return v @ np.diag(w) @ v.conj().T
+
 class SAKO:
     """
     Spectral Analysis for Koopman Operators
@@ -24,8 +32,11 @@ class SAKO:
         P1 (np.ndarray): Psi_1
         W (np.ndarray): Weights for the inner product, default is identity matrix
         reps (float): Threshold for the imaginary part of the residual, default is 1e-10
+        etol (float): Tolerance for clipping small eigenvalues in the inner product matrices, default is 1e-13
     """
-    def __init__(self, P0: np.ndarray, P1: np.ndarray, W: np.ndarray = None, reps: float = 1e-10):
+    def __init__(
+            self, P0: np.ndarray, P1: np.ndarray,
+            W: np.ndarray = None, reps: float = 1e-10, etol: float = 1e-13):
         self._P0 = P0
         self._P1 = P1
         self._W  = np.array(W) if W is not None else np.ones(len(P0),)
@@ -50,6 +61,10 @@ class SAKO:
         self._M10 = self._M01.conj().T
         _M11 = (self._P1.conj().T * self._W).dot(self._P1)
         self._M11 = 0.5 * (_M11 + _M11.conj().T)
+
+        if etol is not None:
+            self._M00 = clip_eig(self._M00, tol=etol)
+            self._M11 = clip_eig(self._M11, tol=etol)
 
         self._M0 = self._M00
         self._M1 = self._M01

src/dymad/training/ls_update.py
@@ -101,12 +101,12 @@ def _ls_truncated(A: np.ndarray, b: np.ndarray, params=None) -> Tuple[np.ndarray
     tsvd = params
     V, U = truncated_lstsq(A, b, tsvd=tsvd)
     return V, U
-    
+
 def _ls_sako(A: np.ndarray, b: np.ndarray, params=None) -> Tuple[np.ndarray, np.ndarray]:
     """Using the SAKO object."""
     W = np.linalg.lstsq(A, b, rcond=None)[0]
     _w, _vl, _vr = scaled_eig(W)
-    sako = SAKO(A, b, reps=1e-10)
+    sako = SAKO(A, b, reps=1e-10, etol=1e-13)
     if isinstance(params, list):
         order = params[0]
         remove_one = params[1] if len(params) > 1 else True
@@ -157,7 +157,7 @@ def _ct_truncated_der(dataloader, model, dt, params=None):
 def _ct_full_log(dataloader, model, dt, params=None):
     A, b = get_batch_dt(dataloader, model, dt)
     W = _ls_full(A, b, params)
-    W = spl.logm(W[0]) / dt
+    W = spl.logm(W) / dt
     return (A, b), (W,)
 
 def _ct_truncated_log(dataloader, model, dt, params=None):

tests/test_workflow_sa_lti.py
@@ -33,7 +33,7 @@ trn_kl = [
         {"type": "lift", "fobs": "poly", "Ks": [4, 4]}
     ]
 
-trn_nd = {
+trn_nd1 = {
     "n_epochs": 100,
     "save_interval": 50,
     "load_checkpoint": False,
@@ -49,8 +49,16 @@ trn_nd = {
         "interval": 50,
         "times": 1}
         }
-trn_dt = copy.deepcopy(trn_nd)
-trn_dt["ls_update"]["method"] = "truncated"
+trn_nd2 = copy.deepcopy(trn_nd1)
+trn_nd2["ls_update"]["method"] = "full_log"
+
+trn_dt1 = copy.deepcopy(trn_nd1)
+trn_dt1["ls_update"]["method"] = "full"
+trn_dt2 = copy.deepcopy(trn_nd1)
+trn_dt2["ls_update"]["method"] = "truncated"
+trn_dt3 = copy.deepcopy(trn_nd1)
+trn_dt3["ls_update"]["method"] = "sako"
+
 ref = {
     "n_epochs": 1,
     "save_interval": 1,
@@ -76,11 +84,14 @@ config_path = 'sa_model.yaml'
 dt = 0.5
 
 cfgs = [
-    ('kbf_nd',  KBF,  NODETrainer,     {"model": mdl_kb, "training" : trn_nd}),
-    ('dkbf_nd', DKBF, NODETrainer,     {"model": mdl_kb, "training" : trn_dt}),
-    ('dkbf_ln', DKBF, LinearTrainer,   {"model": mdl_kl, "transform_x" : trn_kl, "training" : trn_ln}),
-    ('dkbf_tr', DKBF, LinearTrainer,   {"model": mdl_kl, "transform_x" : trn_kl, "training" : trn_tr}),
-    ('dkbf_sa', DKBF, LinearTrainer,   {"model": mdl_kl, "transform_x" : trn_kl, "training" : trn_sa}),
+    ('kbf_nd1',  KBF,  NODETrainer,     {"model": mdl_kb, "training" : trn_nd1}),
+    ('kbf_nd2',  KBF,  NODETrainer,     {"model": mdl_kb, "training" : trn_nd2}),
+    ('dkbf_nd1', DKBF, NODETrainer,     {"model": mdl_kb, "training" : trn_dt1}),
+    ('dkbf_nd2', DKBF, NODETrainer,     {"model": mdl_kb, "training" : trn_dt2}),
+    ('dkbf_nd3', DKBF, NODETrainer,     {"model": mdl_kb, "training" : trn_dt3}),
+    ('dkbf_ln',  DKBF, LinearTrainer,   {"model": mdl_kl, "transform_x" : trn_kl, "training" : trn_ln}),
+    ('dkbf_tr',  DKBF, LinearTrainer,   {"model": mdl_kl, "transform_x" : trn_kl, "training" : trn_tr}),
+    ('dkbf_sa',  DKBF, LinearTrainer,   {"model": mdl_kl, "transform_x" : trn_kl, "training" : trn_sa}),
     ]
 
 def train_case(idx, data, path):
@@ -98,18 +109,20 @@ def predict_case(idx, sample, path):
         _prd = prd_func(x_data, t_data)
         _err = np.linalg.norm(_prd - x_data) / np.linalg.norm(x_data)
 
-        if mdl == 'dkbf_tr':
-            assert _err < 0.05
-        if mdl == 'kbf_nd':
+        if mdl in ['kbf_nd1', 'dkbf_nd2']:
             assert _err < 1e-4
+        elif mdl in ['kbf_nd2', 'dkbf_nd1']:
+            assert _err < 0.01
+        elif mdl == 'dkbf_tr':
+            assert _err < 0.05
         else:
             assert _err < 1e-5
 
 def sa_case(idx, path):
     _, MDL, _, _ = cfgs[idx]
-    _s = SpectralAnalysis(MDL, path/f'sa_model.pt', dt=dt, reps=1e-10)
+    _s = SpectralAnalysis(MDL, path/f'sa_model.pt', dt=dt, reps=1e-10, etol=1e-12)
 
-    xs = np.linspace(-1.3, 1.3, 51)
+    xs = np.linspace(-1.3, 1.3, 4)
     gg = np.vstack([xs, xs])
 
     grid, _pss = _s.estimate_ps(gg, mode='disc', method='standard', return_vec=False)
@@ -120,7 +133,7 @@ def sa_case(idx, path):
     def func_obs(x):
         _x1, _x2 = x.T
         return _x1+_x2
-    _s.estimate_measure(func_obs, 6, 0.1, thetas=501)
+    _s.estimate_measure(func_obs, 6, 0.1, thetas=5)
 
 @pytest.mark.parametrize("idx", range(len(cfgs)))
 def test_sa(sa_lti_data, sa_lti_test, env_setup, idx):