Bug fix in DM in Nystrom; vectorized the eigenvec calculation. Bug fix in Manifold GMLS derivative; Added a new manifold class for alternative tree. Added the forward and backward modes for NDR; added pytest.

src/dymad/numerics/manifold.py
@@ -215,6 +215,10 @@ class Manifold:
 
         logger.info(f"Manifold info: {self._Ndat} points of {self._Ndim}-dim, intrinsic {self._Nman}-dim")
 
+    def _tree_query(self, x):
+        _, _i = self._tree.query(x, k=self._Nknn)
+        return _i
+
     def precompute(self):
         logger.info("  Precomputing")
         if self._ifprecomp:
@@ -229,7 +233,7 @@ class Manifold:
                 rems = list(range(1,self._Ndat))
                 curr = 0
                 while len(rems) > 0:
-                    _, _i = self._tree.query(self._data[curr], k=self._Nknn)
+                    _i = self._tree_query(self._data[curr])
                     _T = self._T[curr]
                     for _j in _i:
                         if _j in rems:
@@ -245,13 +249,13 @@ class Manifold:
         logger.info("  Done")
 
     def gmls(self, x, Y, ret_der=False):
-        # y = g(T^T (X-x))
+        # y = g(T^T (x-X))
         #
         # The derivative is approximate, as x impacts the tangent space estimation,
         # but only the gradients on polynomial are included
         #
         # dy = dg/dd * T^T
-        _, _i = self._tree.query(x, k=self._Nknn)
+        _i = self._tree_query(np.atleast_2d(x))
         _T, _V = self._estimate_tangent(x, ret_V=True)
         _B = np.matmul(_V, np.swapaxes(_T, -2, -1))
         _P = self._poly_eval(self._fpsi, _B)
@@ -260,6 +264,8 @@ class Manifold:
         if ret_der:
             _tmp = _C[..., 1:self._Nman+1, :]
             _rder = np.matmul(np.swapaxes(_tmp, -2, -1), _T)
+            if _rder.ndim == 3 and _rder.shape[0] == 1:
+                _rder = _rder[0]
             return _r, _rder
         return _r
 
@@ -281,7 +287,7 @@ class Manifold:
         return _n.squeeze()
 
     def _estimate_tangent_1(self, x, ret_V=False):
-        _, _i = self._tree.query(x, k=self._Nknn)
+        _i = self._tree_query(x)
         _V = self._data[_i] - x[..., None, :]
         _, _, _Vh = np.linalg.svd(_V, full_matrices=False)
         _T = _Vh.conj()[..., :self._Nman, :]
@@ -350,6 +356,30 @@ class Manifold:
         iforit = t["iforit"].item()
         extT = t["extT"].detach().cpu().numpy() if t["extT"] is not None else None
         return cls(data, d, K, g, T, iforit, extT)
+
+class ManifoldAltTree(Manifold):
+    """
+    The only difference from Manifold is that a KDTree on a different set of points is used
+    to find the kNN points for GMLS.
+
+    Specifically, instead of finding kNN points of x from the original data X,
+    we first transform x to z using a given transform function, and then find
+    the kNN points of z from a given set of points Z (with a KDTree built on Z).
+
+    The rest is still the same as Manifold.
+    """
+    def __init__(self, data, d, K=None, g=None, T=None, iforit=False, extT=None,
+                 tree_data=None, tree_transform=None):
+        super().__init__(data, d, K, g, T, iforit, extT)
+
+        _leaf = max(20, self._Nknn)
+        self._tree = sps.KDTree(tree_data, leafsize=_leaf)
+        self._ftree = tree_transform
+
+    def _tree_query(self, x):
+        z = self._ftree(x)
+        _, _i = self._tree.query(z, k=self._Nknn)
+        return _i
 
 class ManifoldAnalytical(Manifold):
     def __init__(self, data, d, K=None, g=None, fT=None):

src/dymad/transform/ndr.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn.manifold as skm
 from typing import Any, List
 
-from dymad.numerics import DM, DMF, Manifold, VBDM
+from dymad.numerics import DM, DMF, Manifold, ManifoldAltTree, VBDM
 from dymad.transform.base import Transform
 
 Array = List[np.ndarray]
@@ -18,8 +18,11 @@ class TransformKernel(Transform):
     Rigorously, the decoding would require solving the pre-image problem.
     Here we implement some approximate methods to bypass the pre-image solver:
 
-        - Pseudo-inverse
+        - Pseudo-inverse (NOT recommended)
         - (modified) Generalized Moving Least Squares (GMLS)
+
+    When using GMLS, the forward and backward modes are computed with manifold constraints,
+    as a result, the modes are tangent to the manifold in data and latent spaces.
 
     Args:
         edim: Embedding dimension.
@@ -42,7 +45,6 @@ class TransformKernel(Transform):
 
         self._ndr   = None
         self._tree  = None
-        self._man   = None
         self._C     = None
 
     def _prepare_inverse(self):
@@ -51,8 +53,13 @@ class TransformKernel(Transform):
         """
         _inv = self._inv.lower()
         if _inv == "gmls":
-            self._man = Manifold(self._Z, self._order, K=self._Knn, g=self._Kphi, T=self._Kphi)
+            # Backward modes
+            self._man_bck = Manifold(self._Z, self._order, K=self._Knn, g=self._Kphi, T=self._Kphi)
             self.inverse_transform = self._gmls
+            # Forward modes
+            self._man_for = ManifoldAltTree(
+                self._X, self._order, K=self._Knn, g=self._Kphi, T=self._Kphi,
+                tree_data=self._Z, tree_transform=lambda x: self.transform([x])[0])
         elif _inv == "pinv":
             self._C = np.linalg.pinv(self._Z, rcond=self._rcond, hermitian=False).dot(self._X)
             self.inverse_transform = self._pinv
@@ -63,7 +70,7 @@ class TransformKernel(Transform):
         return [_Z.dot(self._C) for _Z in Z]
 
     def _gmls(self, Z):
-        return [self._man.gmls(_Z, self._X) for _Z in Z]
+        return [self._man_bck.gmls(_Z, self._X) for _Z in Z]
 
     def fit(self, X: Array) -> None:
         """"""
@@ -77,16 +84,31 @@ class TransformKernel(Transform):
 
     def transform(self, X: Array) -> Array:
         """"""
-        _res = [self._ndr.transform(_X) for _X in X]
+        _res = [self._ndr.transform(np.atleast_2d(_X)) for _X in X]
         return _res
 
     def get_forward_modes(self, ref=None, **kwargs) -> np.ndarray:
-        """"""
-        raise NotImplementedError("TransformKernel does not implement get_forward_modes.")
+        """
+        We use a specialized GMLS for forward modes.
+        
+        First map ref to latent space, and find the kNN points there;
+        this is because the metric of data space can be more distorted than the latent space.
+        Then, the corresponding kNN points in the original space are found.
+        Finally, the derivative of GMLS at ref is returned, computed using the above modified kNN points.
+        """
+        modes = self._man_for.gmls(ref, self._Z, ret_der=True)[1]
+        return modes
 
     def get_backward_modes(self, ref=None, **kwargs) -> np.ndarray:
-        """"""
-        raise NotImplementedError("TransformKernel does not implement get_backward_modes.")
+        """
+        If pinv is used, return the pseudo-inverse matrix.
+        If gmls is used, the derivative of GMLS at ref is returned.
+        """
+        if self._C is not None:
+            return self._C
+        else:
+            modes = self._man_bck.gmls(ref, self._X, ret_der=True)[1]
+            return np.swapaxes(modes, -2, -1)
 
     def state_dict(self) -> dict[str, Any]:
         """"""

tests/test_assert_trans_mode.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pytest
 
-from dymad.numerics import complex_step
+from dymad.numerics import central_diff, complex_step, Manifold
 from dymad.transform import make_transform
 
 def check_data(out, ref, label=''):
@@ -24,7 +25,7 @@ Xs = [
 Xn = np.array(
     [1.32, 2.4])
 
-def test_modes():
+def test_modes_const():
     # ----------
     # Initialize
     mktr = make_transform([
@@ -48,4 +49,111 @@ def test_modes():
     modes_c = complex_step(backward, Xn).T
     assert np.allclose(modes_f, modes_c), f"Backward modes failed: {modes_f} != {modes_c}"
 
-test_modes()
+def run_modes_ndr(N = 1000, skp = 100, ndr = 'dm'):
+    theta = np.linspace(0, 2 * np.pi * (N - 1) / N, N).reshape(-1, 1)
+    x = np.hstack((2*np.cos(theta), np.sin(theta), np.sin(theta)**3))
+
+    # Embedding
+    if ndr == 'dm':
+        trn_ndr = {
+            "type" : "dm",
+            "edim": 3,
+            "Knn" : 15,
+            "Kphi": 4,
+            "inverse": "gmls",
+            "order": 1,
+            "mode": "full"
+        }
+    elif ndr == 'isomap':
+        trn_ndr = {
+            "type" : "isomap",
+            "edim": 2,
+            "Knn" : 15,
+            "Kphi": 4,
+            "inverse": "gmls",
+            "order": 1
+        }
+    trns = make_transform(trn_ndr)
+    trns.fit([x])
+    z = trns.transform([x])[0]
+
+    # Preparation
+    man = Manifold(x, 1, K=15, g=4, T=4)
+    man.precompute()
+    backward = lambda z: trns.inverse_transform([z])[0].squeeze()
+    forward  = lambda x: trns.transform([x])[0].squeeze()
+
+    # Modes
+    modes_fa, err_f = [], []
+    modes_ba, err_b = [], []
+    for idx in range(0, N, skp):
+        P = man._T[idx].T.dot(man._T[idx])
+        modes_fa.append(trns.get_forward_modes(ref=x[idx]))
+        modes_ff = central_diff(forward, x[idx], v=P)
+        err_f.append(np.linalg.norm(modes_fa[-1] - modes_ff) / np.linalg.norm(modes_fa[-1]))
+
+        modes_ba.append(trns.get_backward_modes(ref=z[idx]))
+        modes_bf = central_diff(backward, z[idx]).T
+        err_b.append(np.linalg.norm(modes_ba[-1] - modes_bf) / np.linalg.norm(modes_ba[-1]))
+
+    return (x, z), (modes_fa, err_f), (modes_ba, err_b)
+
+@pytest.mark.parametrize("ndr", ['dm', 'isomap'])
+def test_modes_ndr(ndr):
+    _, (_, err_f), (_, err_b) = run_modes_ndr(200, 50, ndr=ndr)
+
+    if ndr == 'isomap':
+        eps_f = 10.
+    else:
+        eps_f = 4e-4
+
+    assert np.mean(err_f) < eps_f, f"{ndr} forward modes failed: {np.mean(err_f)}"
+    assert np.mean(err_b) < 2e-5, f"{ndr} backward modes failed: {np.mean(err_b)}"
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+    ifgd = 1
+
+    if ifgd:
+        N, skp = 1000, 100
+        (x, z), (modes_fa, err_f), (modes_ba, err_b) = run_modes_ndr(N, skp, ndr='dm')
+        print(np.mean(err_f), np.mean(err_b))
+
+        clrs = ['b', 'r', 'g']
+        def plot_modes(x, z, modes):
+            if z.shape[1] == 2:
+                z = np.hstack((z, np.zeros((z.shape[0], 1))))
+            f = plt.figure()
+            ax = f.add_subplot(111, projection='3d')
+            ax.plot(x[:,0], x[:,1], x[:,2], 'b.')
+            ax.plot(z[:,0], z[:,1], z[:,2], 'k.')
+
+            for idx in range(len(modes)):
+                jdx = idx * skp
+                ln = np.vstack([x[jdx], z[jdx]]).T
+                plt.plot(ln[0], ln[1], ln[2], 'b:')
+
+                m = modes[idx]
+                if m.shape[0] == 2:
+                    m = np.vstack((m, np.zeros((1, m.shape[1]))))
+                if m.shape[1] == 2:
+                    m = np.hstack((m, np.zeros((m.shape[0], 1))))
+                for _i, _m in enumerate(m):
+                    plt.quiver(*x[jdx], *_m, color=clrs[_i])
+                for _i, _m in enumerate(m.T):
+                    plt.quiver(*z[jdx], *_m, color=clrs[_i])
+            plt.axis('equal')
+
+        plot_modes(x, z, modes_fa)
+        plot_modes(x, z, modes_ba)
+
+        f = plt.figure()
+        plt.semilogy(np.arange(0, N, skp), err_f, 'bo', label='forward', markerfacecolor='none')
+        plt.semilogy(np.arange(0, N, skp), err_b, 'r^', label='backward', markerfacecolor='none')
+        plt.xlabel('Index')
+        plt.ylabel('Relative error')
+        plt.legend()
+
+    plt.show()